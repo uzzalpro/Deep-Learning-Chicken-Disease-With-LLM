87b4a070d5a0052edfab035ea5a86e30fc381476
pro <uzzal.dna@gmail.com>
2025-10-26 16:40:51 +0600

    data Prepare base model

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from cnnClassifier import logger
 logger.info("Starting the CNN Classifier application....")
 
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 # from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 from dotenv import load_dotenv
@@ -16,6 +16,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
